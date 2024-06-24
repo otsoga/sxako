7ecb9c254e107fdf6f8f60bb845242a110c0701c
Victor Agosto Brizuela <victor.m.agosto@gmail.com>
2024-06-24 15:01:12 -0400

    some cleanup

--- src/App.js
+++ src/App.js
@@ -5,48 +5,46 @@ import {Chess} from 'chess.js'
               
 function App() {
   const [game, setGame] = useState(new Chess());
-//Let's perform a function on the game state 
  
-function safeGameMutate(modify){
-  setGame((g)=>{
-    const update = {...g}
+function makeMove(modify){
+  setGame((game)=>{
+    const update = {...game};
     modify(update)
     return update;
   })
 }
-//Movement of computer
+
 function makeRandomMove(){
   const possibleMove = game.moves();
 
-  //exit if the game is over 
-
-  if(game.game_over() || game.in_draw() || possibleMove.length === 0) return;
-  //select random move
+  if(game.game_over() || game.in_draw() || possibleMove.length === 0) {
+    return;
+  }
 
   const randomIndex = Math.floor(Math.random() * possibleMove.length);
- //play random move 
- safeGameMutate((game)=>{
-  game.move(possibleMove[randomIndex]);
- })
+  makeMove((game)=>{
+    game.move(possibleMove[randomIndex]);
+  })
 }
-
-//Perform an action when a piece is droped by a user
  
-function onDrop(source,target){
-  let move = null;
-  safeGameMutate((game)=>{
-    move = game.move({
-      from:source,
-      to: target,
-      promotion:'q'
-    })
-})
- //illegal move 
- if(move== null) return false
- //valid move 
- setTimeout(makeRandomMove, 200);
- return true;
-}
+  function onDrop(source,target){
+    let move = null;
+    makeMove((game)=>{
+      move = game.move({
+        from:source,
+        to: target,
+        promotion:'q'
+      })
+  })
+    if (move == null) { 
+      return false
+    }
+
+    // make the computer move
+    setTimeout(makeRandomMove, 200);
+    return true;
+    }
+
   return (
     <div className="app">
       <Chessboard 
