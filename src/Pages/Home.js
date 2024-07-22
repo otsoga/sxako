32dc9814183b1f8575eb149f88c408989d44d32c
Victor Agosto Brizuela <victor.m.agosto@gmail.com>
2024-07-22 18:08:00 -0400

    update  route for tactics trainer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
             <h1>Sxako</h1>
             <BigButtonLink text="Play vs. CPU" to='/play-vs-cpu' />
             {/* <BigButtonLink text="Openings" to='/openings' /> */}
-            <BigButtonLink text="Tactics Trainer" to={'/tactics'} />
+            <BigButtonLink text="Tactics Trainer" to={'/tactics-trainer'} />
             {/* <BigButtonLink text="Endgames" to={'/endgames'} /> */}
         </Box>
     );
